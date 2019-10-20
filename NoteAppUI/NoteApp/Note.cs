676fbf20b197b3d6543c11d74bdb7d56f09eae01
Владислав Зорин <vladik.zorin@yandex.ru>
2019-10-20 12:28:38 +0700

    Создание класса и написание одной проверки

---
+++ NoteAppUI/NoteApp/Note.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp
+{
+public class Note
+    {
+        private string _name;
+        public string GetName()
+        {
+            return _name;
+        }
+        public void SetName(string name)
+        {
+            if (name.Length < 50)
+            {
+                throw new ArgumentException("Название должно быть меньше 50 символов, а было " + name);
+            }
+            else
+                _name = name;
+        }
+
+        private string category_notes;
+        private string text_notes;
+        private readonly int creation_time = 1;
+        private int last_modified_time;
+
+    }
+}
