e18de8d4a99284e062750242744e646f83bdf475
Владислав Зорин <vladik.zorin@yandex.ru>
2019-11-04 14:26:48 +0700

    Исправлен класс Progect

--- NoteAppUI/NoteApp/project.cs
+++ NoteAppUI/NoteApp/project.cs
@@ -6,9 +6,22 @@ using System.Threading.Tasks;
 
 namespace NoteApp
 {
+    /// <summary>
+    /// Список List
+    /// </summary>
     public class Project
     {
-        public List<Note> Notes = new List<Note>();
-
+        private List<Note> _listNotes = new List<Note>();
+        public List<Note> ListNote
+        {
+            get
+            {
+                return _listNotes;
+            }
+            set
+            {
+                _listNotes = value;
+            }
+        }
     }
 }
